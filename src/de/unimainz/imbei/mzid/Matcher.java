53241de088ca01d4c77e3d65e552d241f14539b1
Andreas Borg <borga@uni-mainz.de>
2012-05-03 08:33:09 +0000

    Erster Code in Matching-Methode

--- src/de/unimainz/imbei/mzid/Matcher.java
+++ src/de/unimainz/imbei/mzid/Matcher.java
@@ -1,5 +1,8 @@
 package de.unimainz.imbei.mzid;
 
+import java.util.Iterator;
+import java.util.Vector;
+
 /**
  * This class is reponsible for comparing a given patient to those present in the local database.
  * 
@@ -9,13 +12,46 @@ package de.unimainz.imbei.mzid;
 public enum Matcher {
 	instance;
 	
+
+	/** The list of Persons in the database */
+	private Iterable<Person> personList;
+	
+	/** PID-Generator instance */
+	private PIDGenerator pidGen;
+	
 	/**
 	 * Checks if a given Patient is already present in the database.
 	 * 
 	 * @return match's PID; null if none found.
 	 */
 	public PID match(Person a){
-		//TODO: implementieren
-		return null;
+
+		Vector<Person> directMatches = new Vector<Person>();
+
+		for (Person b : personList)
+		{
+			// assert that the persons have the same Characteristics 
+			assert (a.getCharacteristics().keySet().equals(b.getCharacteristics().keySet()));
+			
+			// TODO: Eleganter lösen über Datenbankabfrage
+			// get exact matches (i.e. all Characteristics agree)
+			if (a.getCharacteristics().equals(b.getCharacteristics()))
+				directMatches.add(b);
+			
+			// TODO: Gewichte berechnen, Klassifikation etc.
+		}
+		
+		// Two or more exact matches are not possible (errornous database)
+		assert (directMatches.size() < 2);
+		
+		if (directMatches.size() == 1)
+			return directMatches.elementAt(0).getId();
+
+		// ansonsten: Ausgefeilteres Matching
+		
+		// Wenn nichts gefunden: Neuen PID zurückgeben
+		return new PID(pidGen.getNextPIDString());
+		// TODO: Zähler in der Datenbank hochzählen
+	
 	}
 }
