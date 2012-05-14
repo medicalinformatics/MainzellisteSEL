8f04ec0fad42ecf9bac5e927e32867ddc25c4b66
Andreas Borg <borga@uni-mainz.de>
2012-05-14 08:36:29 +0000

    Normalisierung von Nachnamen vervollständigt und getestet

--- src/de/unimainz/imbei/mzid/GermanLastNameDecomposer.java
+++ src/de/unimainz/imbei/mzid/GermanLastNameDecomposer.java
@@ -1,5 +1,8 @@
 package de.unimainz.imbei.mzid;
 
+import java.util.Set;
+import java.util.HashSet;
+
 /**
  * Decomposition of last name into components (3 by default),
  * with recognition of German components such as "von", "Freiherr" etc.
@@ -11,16 +14,74 @@ public class GermanLastNameDecomposer implements FieldTransformer<PlainTextField
 	
 	private int nCcomponents = 3;
 
-	/** Delimiters to recognize when decomposing Names.
+	/** Delimiters to recognize when decomposing Names as regular expression.
 	 * 
 	 */
-	private static char delimiterChars[] = {' ', '.', ':', ',', ';', '-', '\''};
+	private String delimiters = "[ \\.:,;\\-']+";
 
+	private static String nameParticles[] = {"AL", "AM", "AN", "AUF", 
+		"D","DA", "DE", "DEL", "DELA", "DEM", "DEN", "DER", "DI", "DOS", "DR", "DU", 
+		"EL", "EN", "ET", 
+		"FREIFRAU", "FREIHERR", 
+		"GRAEFIN", "GRAF", 
+		"LA", "LE", 
+		/*"MAC",*/ "MC", "MED", 
+		"O", 
+		"PD", "PROF", 
+		"SR", 
+		"UND", 
+		"V", "VAN", "VO", "VOM", "VON", 
+		"Y", 
+		"ZU", "ZUM", "ZUR" };
 	
+	private Set<String> nameParticleSet;
+	private int nComponents = 3;
+	
+	public GermanLastNameDecomposer()
+	{
+		this.nameParticleSet = new HashSet<String>();
+		for (String particle : nameParticles)
+		{
+			nameParticleSet.add(particle);
+		}
+		
+	}
 	public CompoundField<PlainTextField> transform(PlainTextField input)
 	{
-		// TODO möglichst unabhängig von upper / lower case machen
-		return null;
+		CompoundField<PlainTextField> output = new CompoundField<PlainTextField>(nComponents); 
+		String substrings[] = input.getValue().split(delimiters);
+		StringBuffer particles = new StringBuffer();
+		StringBuffer otherComponents = new StringBuffer(); // collects all components > nComponents
+		
+		int i = 0;
+		for (String thisSubstr : substrings)
+		{
+			// collect name particles ("von", "zu") separately
+			if (nameParticleSet.contains(thisSubstr.toUpperCase()))
+			{				
+				if (particles.length() > 0) particles.append(" ");
+				particles.append(thisSubstr);
+				continue;
+			}
+			// Collect other components
+			if (i < nComponents - 1)
+			{
+				output.setValueAt(i, new PlainTextField(thisSubstr));
+				i++;
+			} else {
+				otherComponents.append(" ");
+				otherComponents.append(thisSubstr);
+			}
+		}
+		// fill remaining fields with empty Strings
+		for (;i < nComponents - 1; i++)
+			output.setValueAt(i, new PlainTextField(""));
+		// add particles to last component
+		if (otherComponents.length() > 0)
+			otherComponents.append(" ");
+		otherComponents.append(particles);
+		output.setValueAt(nCcomponents - 1, new PlainTextField(otherComponents.toString()));
+		return output;
 		
 	}
 	
---
+++ test/de/unimainz/imbei/mzid/test/TestPSXLastNameNormalizer.java
@@ -0,0 +1,49 @@
+package de.unimainz.imbei.mzid.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import de.unimainz.imbei.mzid.CompoundField;
+import de.unimainz.imbei.mzid.PSXFirstNameNormalizer;
+import de.unimainz.imbei.mzid.PSXLastNameNormalizer;
+import de.unimainz.imbei.mzid.PlainTextField;
+
+public class TestPSXLastNameNormalizer {
+
+	private String testcases[] =
+		{
+			"Müller-Lüdenscheidt",
+			"Dr. Klöbner",
+			"O'Brien",
+			"Freiherr von und zu Guttenberg",
+			"Guttenberg, Freiherr von und zu",
+			"von Braunschweig-Wolfenbüttel "
+			
+		};
+	
+	private String expected[][] =
+		{
+			{ "MUELLER", "LUEDENSCHEIDT", "" },
+			{ "KLOEBNER", "", "DR"},
+			{ "BRIEN", "", "O" },
+			{ "GUTTENBERG", "", "FREIHERR VON UND ZU"},			
+			{ "GUTTENBERG", "", "FREIHERR VON UND ZU"},
+			{ "BRAUNSCHWEIG", "WOLFENBUETTEL", "VON"}
+		};
+	
+	@Test
+	public void test() {
+		PSXLastNameNormalizer lastNameNormalizer = new PSXLastNameNormalizer();
+		CompoundField<PlainTextField> expectedField = new CompoundField<PlainTextField>(3);
+		for (int i = 0; i < testcases.length; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				expectedField.setValueAt(j, new PlainTextField(expected[i][j]));
+			}
+			assertEquals(expectedField, lastNameNormalizer.transform(new PlainTextField(testcases[i])));
+		}
+	}
+
+}
