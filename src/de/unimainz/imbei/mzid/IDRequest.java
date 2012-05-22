c461dd80d828583c99eeed8f3d4c9ffac55fcb40
Andreas Borg <borga@uni-mainz.de>
2012-05-22 08:41:26 +0000

    Feld assignedPatient zu IDRequest hinzugefügt

--- src/de/unimainz/imbei/mzid/IDRequest.java
+++ src/de/unimainz/imbei/mzid/IDRequest.java
@@ -11,15 +11,32 @@ public class IDRequest {
 	private Map<String, Field<?>> inputFields;
 	/** Type of the requested ID */
 	private String idType;
-	/** The match result, including the assigned patient */
+	/** The match result, including the matched patient */
 	private MatchResult matchResult;
+	/** The patient object that was actually assigned. In case of a match this is usually equal
+	 * to matchResult.patient.
+	 */
+	private Patient assignedPatient;
+
 	
+	
+	/**
+	 * @param inputFields
+	 * @param idType
+	 * @param matchResult
+	 * @param assignedPatient
+	 */
 	public IDRequest(Map<String, Field<?>> inputFields, String idType,
-			MatchResult matchResult) {
+			MatchResult matchResult, Patient assignedPatient) {
 		super();
 		this.inputFields = inputFields;
 		this.idType = idType;
 		this.matchResult = matchResult;
+		this.assignedPatient = assignedPatient;
+	}
+
+	public Patient getAssignedPatient() {
+		return assignedPatient;
 	}
 
 	public Map<String, Field<?>> getInputFields() {
