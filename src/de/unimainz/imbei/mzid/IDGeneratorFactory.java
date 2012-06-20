63bc027985ed2114e6ed815d6be2f13de0c8ee1f
Andreas Borg <borga@uni-mainz.de>
2012-06-20 11:51:20 +0000

    Parameter für PID-Generator aus Config lesen

--- src/de/unimainz/imbei/mzid/IDGeneratorFactory.java
+++ src/de/unimainz/imbei/mzid/IDGeneratorFactory.java
@@ -4,14 +4,29 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import de.unimainz.imbei.mzid.dto.Persistor;
 
 public enum IDGeneratorFactory {
 	instance;
 	
 	private final Map<String, IDGenerator<? extends ID>> generators;
-	
+
 	private IDGeneratorFactory() {
+		Logger logger = Logger.getLogger(IDGeneratorFactory.class);
+		// TODO mehrere Generatoren aus Config lesen
+		try {
+			int k1 = Integer.parseInt(Config.instance.getProperty("idgenerator.pid.k1"));
+			int k2 = Integer.parseInt(Config.instance.getProperty("idgenerator.pid.k2"));
+			int k3 = Integer.parseInt(Config.instance.getProperty("idgenerator.pid.k3"));
+			int rndwidth = Integer.parseInt(Config.instance.getProperty("idgenerator.pid.rndwidth"));
+			
+		} catch (Exception e)
+		{
+			logger.fatal("Parsing of PID generator configuration failed", e);
+		}
+		
 		PIDGenerator pidgen = PIDGenerator.init(1, 2, 3, 0);
 		IDGeneratorMemory mem = Persistor.instance.getIDGeneratorMemory("pid");
 		if (mem == null)
@@ -21,6 +36,8 @@ public enum IDGeneratorFactory {
 		HashMap<String, IDGenerator<? extends ID>> temp = new HashMap<String, IDGenerator<? extends ID>>();
 		temp.put(pidgen.getIdType(), pidgen);
 		generators = Collections.unmodifiableMap(temp);
+		
+		logger.info("ID generators initialized successfully");
 	}
 	
 	public IDGenerator<? extends ID> getFactory(String idType){
