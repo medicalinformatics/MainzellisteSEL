67f237169b44cf2f6315a255ed68720de0ed92b8
Andreas Borg <borga@uni-mainz.de>
2014-09-26 14:58:46 +0000

    Korrekturen zu PROB-117: Cross-Origin Resource Sharing

--- src/de/pseudonymisierung/mainzelliste/CorsResponseFilter.java
+++ src/de/pseudonymisierung/mainzelliste/CorsResponseFilter.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2013 Martin Lablans, Andreas Borg, Frank Ückert
+ * Contact: info@mainzelliste.de
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free 
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * If you modify this Program, or any covered work, by linking or combining it 
+ * with Jersey (https://jersey.java.net) (or a modified version of that 
+ * library), containing parts covered by the terms of the General Public 
+ * License, version 2.0, the licensors of this Program grant you additional 
+ * permission to convey the resulting work.
+ */
 package de.pseudonymisierung.mainzelliste;
 
 import java.io.IOException;
@@ -8,16 +33,33 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Adds header "Access-Control-Allow-Origin" for Cross-origin resource sharing
+ * 
+ * If an incoming request includes the header "Origin", the value of it is
+ * checked against a list of configured hosts (see {@link Config#originAllowed(String)}).
+ * If the host is listed as an allowed origin, the header "Access-Control-Allow-Origin" 
+ * in the response is set to this value. 
+ * @author Andreas Borg
+ *
+ */
 public class CorsResponseFilter implements Filter {
 
+	/**
+	 * Not used in this implementation.
+	 */
 	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void init(FilterConfig arg0) throws ServletException {}
+
+	/**
+	 * Not used in this implementation.
+	 */
+	@Override
+	public void destroy() {}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
@@ -31,13 +73,5 @@ public class CorsResponseFilter implements Filter {
 				httpResponse.addHeader("Access-Control-Allow-Origin", origin);
 		}
 		chain.doFilter(request, response);
-		
 	}
-
-	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
