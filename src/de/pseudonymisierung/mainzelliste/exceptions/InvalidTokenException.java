376758285bd0b98dc84634db3898309b57b36026
Andreas Borg <borga@uni-mainz.de>
2015-02-19 15:26:08 +0000

    Fehlermeldung von InvalidTokenException per getMessage zugänglich machen.

--- src/de/pseudonymisierung/mainzelliste/exceptions/InvalidTokenException.java
+++ src/de/pseudonymisierung/mainzelliste/exceptions/InvalidTokenException.java
@@ -7,7 +7,14 @@ import javax.ws.rs.core.Response.Status;
 // TODO: Statuscode einfügen. Lohnt dann eigene Exception noch?
 public class InvalidTokenException extends WebApplicationException {
 
+	private String message; 
+	
 	public InvalidTokenException(String message) {
 		super(Response.status(Status.BAD_REQUEST).entity(message).build());
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
 	}
 }
