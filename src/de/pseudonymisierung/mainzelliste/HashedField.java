7cca94af101aeeefdaab5004bb1e5e856d37e87d
Andreas Borg <borga@uni-mainz.de>
2015-06-08 16:34:56 +0000

    Angleichungen an DKTK-Branch

--- src/de/pseudonymisierung/mainzelliste/HashedField.java
+++ src/de/pseudonymisierung/mainzelliste/HashedField.java
@@ -47,7 +47,7 @@ public class HashedField extends Field<BitSet>{
 
 	/**	The value is encoded as a String to allow storage in a database. */
 	@Column(length = BloomFilterTransformer.hashLength)
-	private String value;
+	protected String value;
 	
 	/**
 	 * Conversion of the String representation of a bit string to a BitSet.
@@ -96,7 +96,7 @@ public class HashedField extends Field<BitSet>{
 		}
 		return result.toString();
 	}
-	
+
 	/**
 	 * Create an empty instance.
 	 */
