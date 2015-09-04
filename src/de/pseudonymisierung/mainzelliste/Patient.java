a0cf38f3b274c8a786432cfed1ba6af964061b9e
Andreas Borg <borga@uni-mainz.de>
2015-09-04 08:07:27 +0000

    Annotation @Lob statt festem Datenbanktyp "text" (zu PROB-209)

--- src/de/pseudonymisierung/mainzelliste/Patient.java
+++ src/de/pseudonymisierung/mainzelliste/Patient.java
@@ -37,11 +37,13 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.PostLoad;
 import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlRootElement;
+
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jettison.json.JSONException;
@@ -141,7 +143,8 @@ public class Patient {
 	 * 
 	 * @see #fields
 	 */
-	@Column(columnDefinition = "text", length = -1)
+	//@Column(columnDefinition = "text", length = -1)
+	@Lob
 	@JsonIgnore
 	private String fieldsString;
 
