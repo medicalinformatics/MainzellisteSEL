f337fd8d34be0f2a4a145cd6daea03874aea1f0d
Andreas Borg <borga@uni-mainz.de>
2016-02-25 08:39:45 +0000

    Transformator zum Trimmen von Zeichenketten

---
+++ src/de/pseudonymisierung/mainzelliste/matcher/StringTrimmer.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package de.pseudonymisierung.mainzelliste.matcher;
+
+import de.pseudonymisierung.mainzelliste.PlainTextField;
+
+/**
+ * Removes leading and trailing whitespace from character strings.
+ */
+public class StringTrimmer extends FieldTransformer<PlainTextField, PlainTextField> {
+
+	@Override
+	public PlainTextField transform(PlainTextField input) {
+		return new PlainTextField(input.getValue().trim());
+	}
+
+	@Override
+	public Class<PlainTextField> getInputClass() {
+		return PlainTextField.class;
+	}
+
+	@Override
+	public Class<PlainTextField> getOutputClass() {
+		return PlainTextField.class;
+	}
+
+}
